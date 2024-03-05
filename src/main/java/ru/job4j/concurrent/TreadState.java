c47d475bdac5a0a601471c6a84a239d7f7059d21
SergeyDevStart <vasenev.front@gmail.com>
2024-03-05 11:55:56 +0700

    3. Состояние нити. [#229175 #484524] fix

--- src/main/java/ru/job4j/concurrent/TreadState.java
+++ src/main/java/ru/job4j/concurrent/TreadState.java
@@ -6,7 +6,7 @@ public class TreadState {
         Thread second = new Thread(() -> { });
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getName());
             System.out.println(second.getName());
         }
