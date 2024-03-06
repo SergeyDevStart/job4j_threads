3f75157589e94c6ddd138576f262e6a4a28ed2cc
SergeyDevStart <vasenev.front@gmail.com>
2024-03-06 20:00:34 +0700

    Пример прерывания блокированной нити

--- src/main/java/ru/job4j/concurrent/ThreadStop.java
+++ src/main/java/ru/job4j/concurrent/ThreadStop.java
@@ -2,14 +2,23 @@ package ru.job4j.concurrent;
 
 public class ThreadStop {
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(() -> {
-            int count = 0;
-            while (!Thread.currentThread().isInterrupted()) {
-                System.out.println(count++);
-            }
-        });
-        thread.start();
+        Thread progress = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println("start ...");
+                            Thread.sleep(10000);
+                        } catch (InterruptedException e) {
+                            System.out.println(Thread.currentThread().isInterrupted());
+                            System.out.println(Thread.currentThread().getState());
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        progress.start();
         Thread.sleep(1000);
-        thread.interrupt();
+        progress.interrupt();
+        progress.join();
     }
 }
