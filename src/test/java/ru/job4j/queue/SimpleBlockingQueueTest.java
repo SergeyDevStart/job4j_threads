bbf272addf07f229fbf6e91b4ed7fae3c2cdd8ee
SergeyDevStart <vasenev.front@gmail.com>
2024-03-27 05:37:59 +0700

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
@@ -2,38 +2,78 @@ package ru.job4j.queue;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 
 class SimpleBlockingQueueTest {
     @Test
     public void whenOffer3ValueAndPool3ValueThenSuccessfully() throws InterruptedException {
-        var queue = new SimpleBlockingQueue<Integer>(3);
-        List<Integer> result = new ArrayList<>();
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1000);
+        final List<Integer> dataList = IntStream.range(0, 1000)
+                .boxed().collect(Collectors.toList());
         Thread producer = new Thread(() -> {
-            for (int index = 0; index < 3; index++) {
+            for (int index = 0; index < 1000; index++) {
                 try {
                     queue.offer(index);
                 } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    Thread.currentThread().interrupt();
                 }
             }
         });
         Thread consumer = new Thread(() -> {
-            for (int index = 0; index < 3; index++) {
+            while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
                 try {
-                    result.add(queue.poll());
+                    buffer.add(queue.poll());
                 } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    Thread.currentThread().interrupt();
                 }
             }
         });
         producer.start();
         consumer.start();
         producer.join();
+        consumer.interrupt();
         consumer.join();
-        assertThat(result).containsExactly(0, 1, 2);
+        assertThat(buffer).containsAll(dataList);
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i < 5; i++) {
+                            queue.offer(i);
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).containsExactly(0, 1, 2, 3, 4);
     }
 }
